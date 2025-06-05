4b1bfdc7e6cf2d73252a5eba7510b7651c7bd8c3
K20QuickFast237 <nanguedevops@gmail.com>
2025-06-05 16:25:02 +0100

    removed parameter to stream_users function

--- python-generators-0x00/0-stream_users.py
+++ python-generators-0x00/0-stream_users.py
@@ -1,6 +1,9 @@
 
+db = __import__('seed')
 # Function that uses a generator to fetch rows one by one from the user's table
-def stream_users(connection):
+# def stream_users(connection):
+def stream_users():
+    connection = db.connect_to_prodev()
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM user_data;")
     while True:
--- python-generators-0x00/main.py
+++ python-generators-0x00/main.py
@@ -27,5 +27,5 @@ if connection:
 streamer = __import__('0-stream_users')
 
 # iterate over the generator function and print only the first 6 rows
-for user in islice(streamer.stream_users(connection), 6):
+for user in islice(streamer.stream_users(), 6):
     print(user)
